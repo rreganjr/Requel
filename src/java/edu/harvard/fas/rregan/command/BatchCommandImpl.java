b4113073f2b2855776f64b78381a6721cfcf8684
Ron Regan <11188410+rreganjr@users.noreply.github.com>
2009-05-02 09:40:16 +0000

    update copyright and license in all source files.

--- src/java/edu/harvard/fas/rregan/command/BatchCommandImpl.java
+++ src/java/edu/harvard/fas/rregan/command/BatchCommandImpl.java
@@ -1,8 +1,23 @@
 /*
  * $Id: BatchCommandImpl.java,v 1.1 2008/12/13 00:41:04 rregan Exp $
- * Copyright (c) 2008 Ron Regan Jr. All Rights Reserved.
+ * Copyright 2008, 2009 Ron Regan Jr. All Rights Reserved.
+ * This file is part of Requel - the Collaborative Requirments
+ * Elicitation System.
+ *
+ * Requel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Requel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Requel. If not, see <http://www.gnu.org/licenses/>.
+ *
  */
-
 package edu.harvard.fas.rregan.command;
 
 import java.util.Collection;
@@ -12,7 +27,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-
 
 /**
  * Execute a series of commands. If execution fails the results in getCommands()
