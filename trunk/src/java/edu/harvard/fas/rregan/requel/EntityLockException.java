82c992fff67dce0a89006d76ee718b5f8f0aea8b
rreganjr <rreganjr@09e90ae9-a4af-41e9-be73-60e704c5c94f>
2009-05-02 09:53:40 +0000

    update copyright and license in all source files.

--- trunk/src/java/edu/harvard/fas/rregan/requel/EntityLockException.java
+++ trunk/src/java/edu/harvard/fas/rregan/requel/EntityLockException.java
@@ -1,8 +1,23 @@
 /*
  * $Id: EntityLockException.java,v 1.3 2008/12/18 02:01:54 rregan Exp $
- * Copyright (c) 2008 Ron Regan Jr. All Rights Reserved.
+ * Copyright 2008, 2009 Ron Regan Jr. All Rights Reserved.
+ * This file is part of Requel - the Collaborative Requirments
+ * Elicitation System.
+ *
+ * Requel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Requel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Requel. If not, see <http://www.gnu.org/licenses/>.
+ *
  */
-
 package edu.harvard.fas.rregan.requel;
 
 import edu.harvard.fas.rregan.repository.EntityException;
@@ -26,7 +41,8 @@ public class EntityLockException extends EntityException {
 	public static EntityLockException staleEntity(Class<?> entityType, Object entity,
 			EntityExceptionActionType actionType) {
 		return new EntityLockException(entityType, entity, null, null, actionType,
-				MSG_STALE_ENTITY, (entityType == null ? "Unknown Type":entityType.getSimpleName()));
+				MSG_STALE_ENTITY,
+				(entityType == null ? "Unknown Type" : entityType.getSimpleName()));
 	}
 
 	/**
