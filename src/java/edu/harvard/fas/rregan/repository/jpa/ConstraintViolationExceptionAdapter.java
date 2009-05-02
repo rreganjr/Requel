7212a390a452cf5cbdf64e875835e2ac40b13ba1
Ron Regan <11188410+rreganjr@users.noreply.github.com>
2009-05-02 09:42:40 +0000

    update copyright and license in all source files.

--- src/java/edu/harvard/fas/rregan/repository/jpa/ConstraintViolationExceptionAdapter.java
+++ src/java/edu/harvard/fas/rregan/repository/jpa/ConstraintViolationExceptionAdapter.java
@@ -1,8 +1,23 @@
 /*
  * $Id: ConstraintViolationExceptionAdapter.java,v 1.1 2008/12/13 00:41:15 rregan Exp $
- * Copyright (c) 2008 Ron Regan Jr. All Rights Reserved.
+ * Copyright 2008, 2009 Ron Regan Jr. All Rights Reserved.
+ * This file is part of Requel - the Collaborative Requirments
+ * Elicitation System.
+ *
+ * Requel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Requel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Requel. If not, see <http://www.gnu.org/licenses/>.
+ *
  */
-
 package edu.harvard.fas.rregan.repository.jpa;
 
 import org.hibernate.exception.ConstraintViolationException;
@@ -37,10 +52,12 @@ public class ConstraintViolationExceptionAdapter implements EntityExceptionAdapt
 	public EntityException convert(Throwable original, Class<?> entityType, Object entity,
 			EntityExceptionActionType actionType) {
 		if (entityType == null) {
-			if (entity == null ) {
+			if (entity == null) {
 				ConstraintViolationException cve = (ConstraintViolationException) original;
-				if (MySQLIntegrityConstraintViolationException.class.equals(cve.getCause().getClass())) {
-					MySQLIntegrityConstraintViolationException icve = (MySQLIntegrityConstraintViolationException) cve.getCause();
+				if (MySQLIntegrityConstraintViolationException.class.equals(cve.getCause()
+						.getClass())) {
+					MySQLIntegrityConstraintViolationException icve = (MySQLIntegrityConstraintViolationException) cve
+							.getCause();
 					return EntityException.uniquenessConflict(icve, icve.getMessage());
 				}
 			}
