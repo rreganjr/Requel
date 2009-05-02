e470ad83d4df370bf8500785aa73e591b75b8fa9
rreganjr <rreganjr@09e90ae9-a4af-41e9-be73-60e704c5c94f>
2009-05-02 09:58:57 +0000

    update copyright and license in all source files.

--- trunk/src/java/edu/harvard/fas/rregan/requel/ui/project/AddActorToActorContainerController.java
+++ trunk/src/java/edu/harvard/fas/rregan/requel/ui/project/AddActorToActorContainerController.java
@@ -1,6 +1,22 @@
 /*
  * $Id: AddActorToActorContainerController.java,v 1.3 2008/12/13 00:41:10 rregan Exp $
- * Copyright (c) 2008 Ron Regan Jr. All Rights Reserved.
+ * Copyright 2008, 2009 Ron Regan Jr. All Rights Reserved.
+ * This file is part of Requel - the Collaborative Requirments
+ * Elicitation System.
+ *
+ * Requel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Requel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Requel. If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 package edu.harvard.fas.rregan.requel.ui.project;
 
@@ -60,8 +76,10 @@ public class AddActorToActorContainerController extends AbstractRequelCommandCon
 					command.setActorContainer(actorContainer);
 					command = getCommandHandler().execute(command);
 					fireEvent(new UpdateEntityEvent(this, null, command.getActor()));
-					// TODO: this may not be needed because the update listeners for things that
-					// are actor containers will probably pickup the previous event and get the
+					// TODO: this may not be needed because the update listeners
+					// for things that
+					// are actor containers will probably pickup the previous
+					// event and get the
 					// updated actor container from the actor.
 					fireEvent(new UpdateEntityEvent(this, null, command.getActorContainer()));
 				} catch (Exception e) {
--- trunk/src/java/edu/harvard/fas/rregan/requel/ui/project/AddStoryToStoryContainerController.java
+++ trunk/src/java/edu/harvard/fas/rregan/requel/ui/project/AddStoryToStoryContainerController.java
@@ -1,6 +1,22 @@
 /*
  * $Id: AddStoryToStoryContainerController.java,v 1.3 2008/12/13 00:41:05 rregan Exp $
- * Copyright (c) 2008 Ron Regan Jr. All Rights Reserved.
+ * Copyright 2008, 2009 Ron Regan Jr. All Rights Reserved.
+ * This file is part of Requel - the Collaborative Requirments
+ * Elicitation System.
+ *
+ * Requel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Requel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Requel. If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 package edu.harvard.fas.rregan.requel.ui.project;
 
@@ -60,8 +76,10 @@ public class AddStoryToStoryContainerController extends AbstractRequelCommandCon
 					command.setStoryContainer(storyContainer);
 					command = getCommandHandler().execute(command);
 					fireEvent(new UpdateEntityEvent(this, null, command.getStory()));
-					// TODO: this may not be needed because the update listeners for things that
-					// are story containers will probably pickup the previous event and get the
+					// TODO: this may not be needed because the update listeners
+					// for things that
+					// are story containers will probably pickup the previous
+					// event and get the
 					// updated story container from the story.
 					fireEvent(new UpdateEntityEvent(this, null, command.getStoryContainer()));
 				} catch (Exception e) {
