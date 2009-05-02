f569fa2a4c67625043abdfdbf5e80f56f4d5cb59
rreganjr <rreganjr@09e90ae9-a4af-41e9-be73-60e704c5c94f>
2009-05-02 09:40:16 +0000

    update copyright and license in all source files.

--- trunk/src/java/edu/harvard/fas/rregan/command/AbstractCommand.java
+++ trunk/src/java/edu/harvard/fas/rregan/command/AbstractCommand.java
@@ -1,6 +1,22 @@
 /*
  * $Id: AbstractCommand.java,v 1.1 2008/12/13 00:41:02 rregan Exp $
- * Copyright (c) 2008 Ron Regan Jr. All Rights Reserved.
+ * Copyright 2008, 2009 Ron Regan Jr. All Rights Reserved.
+ * This file is part of Requel - the Collaborative Requirments
+ * Elicitation System.
+ *
+ * Requel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Requel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Requel. If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 package edu.harvard.fas.rregan.command;
 
@@ -19,7 +35,7 @@ public abstract class AbstractCommand implements Command {
 	protected AbstractCommand(Repository repository) {
 		this.repository = repository;
 	}
-	
+
 	protected Repository getRepository() {
 		return repository;
 	}
